3ae9f855d1fc2789cb4cf074740f965c3f0be1dd
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-10-11 14:15:01 -0700

    Add Book constructor function with parameters title, author, pages and read

--- library.js
+++ library.js
@@ -1,5 +1,8 @@
 let myLibrary = [];
 
-function Book() {
-    
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
