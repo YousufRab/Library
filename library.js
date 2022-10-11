caab552764f8045044114c8a22582835d447c9d5
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-10-11 14:27:06 -0700

    Add code to addBookToLibrary function

--- library.js
+++ library.js
@@ -8,5 +8,13 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary () {
-    
+    let newTitle = prompt("What is the book's title?");
+    let newAuthor = prompt("Who is the author?");
+    let newPages = prompt("How many pages is the book? If you are unsure type 0.")
+    let newRead = prompt("Have you read this book? Type 'yes'/'y' to confirm, otherwise type 'no/n'.")
+    if (newRead.toLowerCase() === 'yes' || newRead.toLowerCase() === 'y') {
+        newRead = true;
+    } else { 
+        newRead = false;
+    }
 }
