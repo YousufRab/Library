dc23d159e5b7ec71d44c356e45f405ca12120648
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-10-13 12:35:21 -0700

    Create openForm function for revealing #bookForm

--- library.js
+++ library.js
@@ -48,3 +48,7 @@ function createRadioButton (){
     firstRadioBtn.setAttribute('value', 'readBook');
     firstRadioBtn.setAttribute()
 }
+
+function openForm () {
+    document.getElementById('bookForm').style.display = 'block';
+}
